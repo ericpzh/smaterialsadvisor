0a53cd042749aa0ba64509d50b2ee8012d5cdaa4
ericpzh <zp3@illinois.edu>
2020-02-20 14:04:12 -0600

    Updates

---
+++ precache-manifest.76c3217073458845f59ec8ccc8fe2ad3.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae9a76f7c14d61633e33",
+    "url": "/smaterialsadvisor/static/css/main.22451edb.chunk.css"
+  },
+  {
+    "revision": "ae9a76f7c14d61633e33",
+    "url": "/smaterialsadvisor/static/js/main.ae9a76f7.chunk.js"
+  },
+  {
+    "revision": "2433a973555a5b1e6fc5",
+    "url": "/smaterialsadvisor/static/js/runtime~main.2433a973.js"
+  },
+  {
+    "revision": "8357bddbe5bd12c5940e",
+    "url": "/smaterialsadvisor/static/css/2.b56c3f62.chunk.css"
+  },
+  {
+    "revision": "8357bddbe5bd12c5940e",
+    "url": "/smaterialsadvisor/static/js/2.8357bddb.chunk.js"
+  },
+  {
+    "revision": "17fe988d8d74b1ca47987b7431e1261f",
+    "url": "/smaterialsadvisor/static/media/favicon.17fe988d.ico"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/smaterialsadvisor/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/smaterialsadvisor/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/smaterialsadvisor/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/smaterialsadvisor/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/smaterialsadvisor/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/smaterialsadvisor/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/smaterialsadvisor/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/smaterialsadvisor/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/smaterialsadvisor/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/smaterialsadvisor/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/smaterialsadvisor/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/smaterialsadvisor/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/smaterialsadvisor/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/smaterialsadvisor/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/smaterialsadvisor/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/smaterialsadvisor/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "2b37508b92c1cfead7added58633c035",
+    "url": "/smaterialsadvisor/static/media/Idematapp.2b37508b.csv"
+  },
+  {
+    "revision": "802e7c8a5955e4ba3f431a5113e4ef06",
+    "url": "/smaterialsadvisor/index.html"
+  }
+];
